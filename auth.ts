edd8f39576756cf68141231a58f3ab32313cda21
Brianna Matey <briianna@bu.edu>
2026-04-21 22:56:00 -0400

    second attempt

--- auth.ts
+++ auth.ts
@@ -7,9 +7,17 @@ if (!clientId || !clientSecret) {
     throw new Error("Missing credentials");
 
 }
+console.log(" auth.ts loaded");
+
+console.log(" env check", {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    secret: process.env.GOOGLE_CLIENT_SECRET,
+});
 export const {handlers, auth, signIn, signOut} = NextAuth({
     providers: [Google({
         clientId,
         clientSecret
     })],
 })
+
+console.log(" handlers:", handlers);
