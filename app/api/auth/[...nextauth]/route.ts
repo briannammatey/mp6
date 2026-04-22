530f7b06b7ac0914ea2c2c02c8426f7c1bd6224c
Brianna Matey <briianna@bu.edu>
2026-04-22 13:59:58 -0400

    attempt to fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,6 @@
 import {handlers} from "@/auth"
 
-export const {GET, POST} = handlers
+//export const {GET, POST} = handlers
+
+export const GET = handlers.GET;
+export const POST = handlers.POST;
--- auth.ts
+++ auth.ts
@@ -1,11 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-console.log("GOOGLE CLIENT ID:", process.env.GOOGLE_CLIENT_ID);
 
-console.log("GOOGLE CLIENT ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("NEXTAUTH_SECRET EXISTS:", !!process.env.NEXTAUTH_SECRET);
-console.log("TRUST HOST:", process.env.AUTH_TRUST_HOST);
-console.log("URL:", process.env.NEXTAUTH_URL);
 export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [
         Google({
