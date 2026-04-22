103635daf9c537e6f313f3446dd3e9c88c6b0178
Brianna Matey <briianna@bu.edu>
2026-04-21 23:06:11 -0400

    modified more

--- auth.ts
+++ auth.ts
@@ -1,23 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-const clientId = process.env.GOOGLE_CLIENT_ID;
-const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-if (!clientId || !clientSecret) {
-    throw new Error("Missing credentials");
-
-}
-console.log(" auth.ts loaded");
-
-console.log(" env check", {
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    secret: process.env.GOOGLE_CLIENT_SECRET,
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    providers: [Google],
 });
-export const {handlers, auth, signIn, signOut} = NextAuth({
-    providers: [Google({
-        clientId,
-        clientSecret
-    })],
-})
-
-console.log(" handlers:", handlers);
